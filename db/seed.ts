84bc0ca791e37926b68e4538eab62a36ad21e404
fdreer <francodreer@gmail.com>
2024-04-29 19:07:09 -0300

    astrojs/db to 0.8.8

--- db/seed.ts
+++ db/seed.ts
@@ -1,5 +1,7 @@
 import { db, Likes } from 'astro:db'
 
 export default async function seed() {
-	await db.insert(Likes).values({ id: 'cv-franco', number: 7 })
+	await db.insert(Likes).values({ id: 'cv-franco', number: 0 })
 }
+
+// npx astro db execute db/seed.ts --remote

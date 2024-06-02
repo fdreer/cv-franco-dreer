6c1b6094fb10c6d2a99cf8249614e12e06649030
fdreer <francodreer@gmail.com>
2024-06-01 22:27:20 -0300

    add vars to project

---
+++ src/data.ts
@@ -0,0 +1,16 @@
+const DATA = {
+	ME: {
+		NAME: 'Franco Dreer',
+		DESCRIPTION: 'Estudiante entusiasta de la tecnología y los negocios',
+		HOME: 'Salta Capital, Argentina'
+	},
+	LINKS: {
+		LINKEDIN: 'https://www.linkedin.com/in/francodreer',
+		MAIL: 'francodreer@gmail.com',
+		GITHUB: 'https://github.com/fdreer',
+		CV_PDF: '/cv_franco_dreer.pdf'
+	},
+	URL_PRODUCTION: 'https://cv-franco-dreer.pages.dev/'
+}
+
+export default DATA

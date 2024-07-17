a982bb8342534658428275d7a3bee52111afeb14
fdreer <francodreer@gmail.com>
2024-07-17 12:19:02 -0300

    change to hybrid mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import cloudflare from '@astrojs/cloudflare'
 import db from '@astrojs/db'
 
 export default defineConfig({
-    output: 'server',
+    output: 'hybrid',
     adapter: cloudflare(),
     integrations: [db()]
 })

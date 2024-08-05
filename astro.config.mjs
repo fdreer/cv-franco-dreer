8a9c309ddc492ce2d16816fc1c601a68a02c90ba
fdreer <francodreer@gmail.com>
2024-08-04 21:09:22 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import db from '@astrojs/db'
 
 export default defineConfig({
     output: 'hybrid',
-    adapter: cloudflare(),
+    adapter: cloudflare({
+        routes: {
+            extend: {
+                include: '/api/likes'
+            }
+        }
+    }),
     integrations: [db()]
 })

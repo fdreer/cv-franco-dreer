057faa73fcf5aa586c4ddba9388067ad3b4b2385
fdreer <francodreer@gmail.com>
2026-08-04 20:18:47 -0300

    chore: borrar los json viejos y los tipos que reemplazan

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,27 +1,3 @@
-type Course = {
-    title: string
-    institution: string
-    timeFrame?: {
-        startDate: string
-        endDate: string
-    }
-    images?: {
-        title: string
-        src: string
-        href?: string
-    }[]
-}
-
-type Formation = {
-    title: string
-    institution: string
-    timeFrame?: {
-        startDate: string
-        endDate: string
-    }
-    description?: string
-}
-
 type CvExperiencia = {
     rol: string
     empresa: string
